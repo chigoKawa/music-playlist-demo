543f195029e655232789e217196b2d27fb7283ad
chigoKawa <kawa.chigozirim@gmail.com>
2021-11-02 22:26:36 +0100

    initial

--- components/config.js
+++
@@ -1,9 +0,0 @@
-// All environment variables will be sourced
-// Do NOT commit this file to source control
-module.exports = {
-    space_id: 'wziypkqxv6cw',
-    delivery_token: 'DknhlrvwueNy9Dh9yRq6UdW9NZucY5jqIOm3Na7qpWk',
-    preview_token: '3g4E3OjWji6-eqImdmEOju7KLhrlUutXaKHEx6fQ4Js',
-    previewSecret: 'testing',
-    environment: 'master'
-}
